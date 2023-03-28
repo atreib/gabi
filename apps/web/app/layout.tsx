977e8555626d678e288aafba88bc2864765e9bc8
Andre Treib <andre.schumacher.treib@gmail.com>
2023-03-27 22:37:38 -0300

    feat: add i18n

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -6,6 +6,10 @@ export default function Layout({ children }: PropsWithChildren) {
     <html>
       <body className="p-8 space-y-8">
         <header>
+          <h2 className="flex space-x-4 pl-[0.15rem] pb-4 text-2xl">
+            <a href="/?lang=en-us">🇺🇸</a>
+            <a href="/?lang=pt-br">🇧🇷</a>
+          </h2>
           <h1>Gabi</h1>
         </header>
         <section>{children}</section>
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,8 +2,15 @@ import { getSymptoms } from "../src/symptoms/services";
 import { Language } from "../src/types";
 import { Examination } from "./examination";
 
-export default function Page() {
-  const language: Language = "ptBR";
+export default function Page({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const { lang } = searchParams;
+  const language: Language = lang === "pt-br" ? "ptBR" : "enUS";
   const symptoms = getSymptoms();
 
   return (
